54ef948dd8a5690eec645dc8aad1c698a2f07424
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-21 19:47:42 +0100

    Add files via upload

---
+++ savoir.py
@@ -0,0 +1,10 @@
+entreprise = lines[2].split(";")
+
+a = sorted(dico.values())
+a = a[::-1]
+
+import operator
+b = sorted(dico.items(), key=itemgetter(1), reverse=True)
+
+
+[c for c,v in d.items() if v==val]
